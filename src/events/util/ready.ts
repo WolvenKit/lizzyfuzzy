335a3e1e8ebe133847e095747ce73a217b478e87
Sebastian Danielzik <moonded@moonded.com>
2024-07-30 17:52:25 +0200

    Check for internet availabiliy

--- src/events/util/ready.ts
+++ src/events/util/ready.ts
@@ -1,14 +1,38 @@
-import { event } from '../../utils';
-const date = new Date().getHours()
-const time = date < 12 ? 'Good Morning' : date < 18 ? 'Good Afternoon' : 'Good Night'
+import { event } from "../../utils";
+import dns from "dns";
+const date = new Date().getHours();
+const time =
+  date < 12 ? "Good Morning" : date < 18 ? "Good Afternoon" : "Good Night";
 
-export default event('ready', async ({ log }, client) => {
-  process.on('uncaughtException', error => {
+export default event("ready", async ({ log }, client) => {
+  process.on("uncaughtException", (error) => {
     console.log(`uncaughtException:  ${error} `);
   });
 
-  log(`${time}! I'm currently logged in as ${client.user.tag} and running on ${client.guilds.cache.size} servers!`);
-  log(`I'm currently in ${client.channels.cache.size} channels and serving ${client.users.cache.size} users!`);
+  log(
+    `${time}! I'm currently logged in as ${client.user.tag} and running on ${client.guilds.cache.size} servers!`
+  );
+  log(
+    `I'm currently in ${client.channels.cache.size} channels and serving ${client.users.cache.size} users!`
+  );
   log(`Run the command "Servers" for a list of servers I'm in!`);
 
+  //  periodicaly check for internet connection
+  dns.resolve("www.google.com", function (err: any) {
+    if (err) {
+      log("No internet connection!");
+    } else {
+      log("Internet connection is available!");
+    }
+  });
+
+  setInterval(() => {
+    dns.resolve("www.google.com", function (err: any) {
+      if (err) {
+        log("No internet connection!");
+      } else {
+        log("Internet connection is available!");
+      }
+    });
+  }, 600000);
 });
