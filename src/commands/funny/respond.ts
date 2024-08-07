ee83c5a26cf320015b5279d56e70175d2e8c96ad
Sebastian Danielzik <moonded@moonded.com>
2024-08-07 20:10:32 +0200

    use github raw content for now

--- src/commands/funny/respond.ts
+++ src/commands/funny/respond.ts
@@ -1,6 +1,7 @@
 import { PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import type { TextChannel } from "discord.js";
 import { command } from "../../utils";
+// import responses from '../../resources/respond.json'
 
 const meta = new SlashCommandBuilder()
   .setName("respond")
@@ -8,46 +9,27 @@ const meta = new SlashCommandBuilder()
   .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild);
 
 export default command(meta, async ({ interaction, client }) => {
-  const responses = [
-    { Message: "The fuck you want?", User: "everyone" },
-    { Message: "I am here to serve you, creator", User: "moonded" },
-    { Message: "I am V", User: "projectv" },
-    { Message: "the bin in the bin", User: "everyone" },
-    {
-      Message: "If I catch zhin outside again, i swear to...",
-      User: "moonded",
-    },
-    {
-      Message: "It looks like you pirated the game - clippy",
-      User: "everyone",
-    },
-    {
-      Message:
-        "I ain't reading all that. I'm happy for u tho. Or sorry that happend",
-      User: "everyone",
-    },
-    {
-      Message: "Have you tried turning it on and off again?",
-      User: "everyone",
-    },
-    { Message: "50$ steam gift card bro", User: "everyone" },
-    { Message: "Reading is hard.", User: "everyone" },
-    { Message: "EVERYONE", User: "everyone" },
-  ];
-
   const user = interaction.member?.user.username;
 
-  function randomResponse() {
+  async function randomResponse() {
+    const data = await fetch(
+      "https://raw.githubusercontent.com/Moonded/responses/main/responses.json"
+    );
+
+    const responses = await data.json();
+
     return responses[Math.floor(Math.random() * responses.length)];
   }
 
-  let response = randomResponse();
+  let response = await randomResponse();
 
   while (response.User !== "everyone" && response.User !== user) {
     response = randomResponse();
   }
 
-  const channel = client.channels.cache.get(interaction.channelId) as TextChannel;
+  const channel = client.channels.cache.get(
+    interaction.channelId
+  ) as TextChannel;
   if (channel) {
     channel.send(response.Message);
   }
