71601f31f52ef4b06489e8421d32e9a49c1feda4
Sebastian Danielzik <moonded@moonded.com>
2024-08-10 15:27:15 +0200

    remove despreciated commands

--- src/commands/misc/index.ts
+++ src/commands/misc/index.ts
@@ -1,9 +1,6 @@
 import { category } from 'utils';
 import who from './who';
-import init from './init';
-import servers from './servers';
 import info from './info';
 import link from './link';
-import dev from './dev';
 
-export default category('Misc', [who, servers, init, info, link, dev]);
+export default category('Misc', [who, info, link]);
