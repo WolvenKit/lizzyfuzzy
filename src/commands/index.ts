32e6e736dba4722eb9fe80d1e2c900b7f5380302
Sebastian Danielzik <moonded@moonded.com>
2024-08-11 19:52:12 +0200

    update versions

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,6 +5,9 @@ import pub from "./public";
 import team from "./team";
 import dev from "./dev";
 
-export default process.env.NODE_ENV === "production"
-  ? [misc, context, funny, pub, team]
-  : [misc, context, funny, pub, team, dev];
+// export default process.env.NODE_ENV === "production"
+//   ? [misc, context, funny, pub, team]
+//   : [misc, context, funny, pub, team, dev];
+
+export default [misc, context, funny, pub, team];
+// export default []
