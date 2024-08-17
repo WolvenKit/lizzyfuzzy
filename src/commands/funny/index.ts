bb286c2edb3599b6f5a674ec43164267c7ad86d6
Sebastian Danielzik <moonded@moonded.com>
2024-08-17 20:04:59 +0200

    disable dev and unfinished commands for prod

--- src/commands/funny/index.ts
+++ src/commands/funny/index.ts
@@ -2,4 +2,4 @@ import { category } from 'utils';
 import respond from './respond';
 import trivia from './trivia';
 
-export default category('funny', [respond, trivia]);
+export default category('funny', [respond]);
