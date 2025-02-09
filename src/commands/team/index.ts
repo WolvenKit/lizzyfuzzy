1bf7399dfdab76ecad7a1824bd103db5460ac487
Moonded <moonded@moonded.com>
2025-02-09 22:32:44 +0100

    add warn command

--- src/commands/team/index.ts
+++ src/commands/team/index.ts
@@ -7,5 +7,6 @@ import clear from "./clear";
 import repeat from "./repeat";
 import info from "./info";
 import clean from "./clean";
+import warn from "./warn";
 
-export default category("Team", [info, repeat, clear, who, link, mark, mute, clean]);
+export default category("Team", [warn, info, repeat, clear, who, link, mark, mute, clean]);
