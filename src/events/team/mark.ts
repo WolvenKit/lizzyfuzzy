b8e1c2dfb50412b56f6dd9c5303d734d81290b5d
Moonded <moonded@moonded.com>
2025-02-09 17:19:18 +0100

    marked user command finnish

--- src/events/team/mark.ts
+++ src/events/team/mark.ts
@@ -1,39 +1,55 @@
 import { errorLog, event } from "utils";
 import { Database } from "bun:sqlite";
-import { Colors, EmbedType, MessageFlags } from "discord.js";
+import { Role, MessageFlags } from "discord.js";
 
-export default event("interactionCreate", async ({ log, client }, interaction) => {
+export default event(
+  "interactionCreate",
+  async ({ log, client }, interaction) => {
     try {
+      if (!interaction.isButton()) return;
 
-        if (!interaction.isButton()) return;
+      const button = interaction.customId.split("_");
 
-        const button = interaction.customId.split("_")
+      if (button[0] === "pirate") {
+        const db = new Database("settings.sqlite");
 
-        if (button[0] === "pirate") {
+        if (button[1] === "mark") {
+          interaction.guild?.members.fetch(button[2]).then((member) => {
+            const role = db
+              .query(
+                `SELECT value FROM settings WHERE key = 'setting_markedMemberRole'`
+              )
+              .get() as { value: string };
 
-            const db = new Database("settings.sqlite");
+            const ValuateRole = interaction.guild?.roles.cache.get(
+              role.value
+            ) as Role;
 
-            if (button[1] === "mark") {
-                interaction.guild?.members.fetch(button[2]).then(member => {
-                    member.roles.add("1335271939513516135")
+            member.roles.add(ValuateRole);
 
+            return interaction.message.edit({ components: [] });
+          });
 
-                    return interaction.message.edit({ components: [] })
-                })
-
-                return interaction.message.edit({ components: [] })
-            }
-            if (button[1] === "ignore") {
-                db.query(`DELETE FROM markedMembers WHERE user = '${button[2]}'`).run();
-                interaction.reply({ content: "Successfully Ignored Marked member!\nMarked member: <@" + button[2] + ">", flags: MessageFlags.Ephemeral })
-
-                return interaction.message.edit({ components: [] })
-            }
+          return interaction.message.edit({ components: [] });
         }
+        if (button[1] === "ignore") {
+          db.query(
+            `DELETE FROM markedMembers WHERE user = '${button[2]}'`
+          ).run();
+          interaction.reply({
+            content:
+              "Successfully Ignored Marked member!\nMarked member: <@" +
+              button[2] +
+              ">",
+            flags: MessageFlags.Ephemeral,
+          });
 
+          return interaction.message.edit({ components: [] });
+        }
+      }
     } catch (error) {
-        errorLog(error);
+      errorLog(error);
     }
-}
+  }
 );
-// Compare this snippet from src/utils/index.ts:    
+// Compare this snippet from src/utils/index.ts:
