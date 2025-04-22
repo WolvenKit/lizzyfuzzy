fea3209540b2ab167a6cc3e0c619b0cb88faf8db
Moonded <moonded@moonded.com>
2025-04-22 16:19:48 +0200

    add new dev command

--- src/commands/dev/index.ts
+++ src/commands/dev/index.ts
@@ -4,5 +4,6 @@ import servers from "./servers";
 import localdb from "./localdb";
 import sync from "./sync";
 import dev from "./dev";
+import components from "./component";
 
-export default category("Dev", [localdb, sync, servers, dev]);
+export default category("Dev", [localdb, sync, servers, dev, components]);
