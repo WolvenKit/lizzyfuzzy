dcb6d76b73b8efdc6e81a325272279a10d36abad
Moonded <moonded@moonded.com>
2025-04-22 16:20:03 +0200

    message doublication checker

--- src/events/doublication/checker.ts
+++ src/events/doublication/checker.ts
@@ -1,12 +1,18 @@
 import { errorLog, event } from "utils";
+import crypto from "crypto";
 
 export default event("messageCreate", async ({ client }, Message) => {
   try {
     if (Message.author.bot) return;
 
+    const Hash = crypto.createHash("sha512", {
+        encoding: "utf8",
+        outputLength: 64,
+    });
+
     const Context = {
       Timestamp: Message.createdTimestamp.toString(),
-      Content: Message.content,
+      Content: Hash.update(Message.content.toLocaleLowerCase().trim().replace(/\s/g, "")).digest("hex"),
       UserId: Message.author.id,
     };
 
@@ -14,15 +20,13 @@ export default event("messageCreate", async ({ client }, Message) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer TOKEN`,
+        "Authorization": `Bearer ${process.env.API_KEY}`,
       },
       body: JSON.stringify(Context),
     })
       .then((response) => response)
       .then((data) => data.text())
       .catch((error) => console.error(error));
-
-    console.log(data)
 
     if (data && data === "true") {
       Message.delete();
