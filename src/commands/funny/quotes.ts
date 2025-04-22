b2b6340c9cd874a550573b7cbacc116e106d61b5
Moonded <moonded@moonded.com>
2025-04-22 20:52:47 +0200

    improved and fixed quote command

--- src/commands/funny/quotes.ts
+++ src/commands/funny/quotes.ts
@@ -3,38 +3,118 @@ import type { TextChannel } from "discord.js";
 import { command, errorLog } from "utils";
 
 const meta = new SlashCommandBuilder()
-  .setName("quotes")
+  .setName("quote")
   .setDescription("quotes")
+  .addNumberOption((option) =>
+    option
+      .setName("quote")
+      .setDescription("Fetch a Quote by its Global ID")
+      .setRequired(false)
+      .setMinValue(1)
+  )
   .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild);
 
-export default command(meta, async ({ interaction }) => {
-  try {
-    const data = await fetch(process.env.API_ENDPOINT + "/bot/quotes/request", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        // authorization: `Bearer ${process.env.BOT_TOKEN}`,
-      },
-      body: JSON.stringify({
-        Responder: interaction.user.username,
-      }),
-    })
-      .then((response) => response)
-      .then((res) => res.json())
-      .catch((e) => {
+export default command(
+  meta,
+  async ({ interaction }) => {
+    try {
+      if (interaction.options.getNumber("quote") !== null) {
+        const quoteId = interaction.options.getNumber("quote");
+
+        const data = await fetch(
+          process.env.API_ENDPOINT + `/bot/quotes?quoteId=${quoteId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.API_KEY}`,
+            },
+          }
+        ).then((data) => data.json());
+
+        if (!data) {
+          return interaction.reply({
+            embeds: [
+              {
+                title: "Error",
+                description: "Failed to fetch quotes",
+                color: 0xff0000,
+              },
+            ],
+            flags: 64,
+          });
+        }
+
+        const quotes = data as {
+          GlobalId: number;
+          Quote: string;
+          Responder: string;
+        };
+
+        const channel = interaction.channel as TextChannel;
+        channel.send({
+          content: quotes.Quote,
+        });
+
         return interaction.reply({
           embeds: [
             {
-              title: "Error",
-              description: "Failed to fetch quotes",
-              color: 0xff0000,
+              title: "Success",
+              description: "Quotes fetched and sent to channel",
+              color: 0x00ff00,
             },
           ],
           flags: 64,
         });
-      });
+      } else {
+        const data = await fetch(
+          process.env.API_ENDPOINT +
+            `/bot/quotes/request?user=${interaction.user.username}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.API_KEY}`,
+            },
+          }
+        ).then((data) => data.json());
 
-    if (!data) {
+        if (!data) {
+          return interaction.reply({
+            embeds: [
+              {
+                title: "Error",
+                description: "Failed to fetch quotes",
+                color: 0xff0000,
+              },
+            ],
+            flags: 64,
+          });
+        }
+
+        const quotes = data[0] as {
+          GlobalId: number;
+          Quote: string;
+          Responder: string;
+        };
+
+        const channel = interaction.channel as TextChannel;
+
+        channel.send({
+          content: quotes.Quote,
+        });
+
+        return interaction.reply({
+          embeds: [
+            {
+              title: "Success",
+              description: "Quotes fetched and sent to channel",
+              color: 0x00ff00,
+            },
+          ],
+          flags: 64,
+        });
+      }
+    } catch (e) {
+      errorLog(e);
       return interaction.reply({
         embeds: [
           {
@@ -46,42 +126,7 @@ export default command(meta, async ({ interaction }) => {
         flags: 64,
       });
     }
-
-    const quotes = data[0] as {
-      GlobalId: number;
-      Quote: string;
-      Responder: string;
-    }
-
-    const channel = interaction.channel as TextChannel;
-
-    channel.send({
-      content: quotes.Quote
-    });
-
-    return interaction.reply({
-      embeds: [
-        {
-          title: "Success",
-          description: "Quotes fetched and sent to channel",
-          color: 0x00ff00,
-        },
-      ],
-      flags: 64,
-    });
-
-
-  } catch (e) {
-    errorLog(e);
-    return interaction.reply({
-      embeds: [
-        {
-          title: "Error",
-          description: "Failed to fetch quotes",
-          color: 0xff0000,
-        },
-      ],
-      flags: 64,
-    });
-  }
-}, false, false);
+  },
+  false,
+  false
+);
